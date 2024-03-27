cc6c26abdefd161dfaaf1965da26c6903b1c866c
Avijit Karmaker <ajkacca@gmail.com>
2024-03-27 21:56:08 +0200

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <>
+    <div>
+      <Header />  
       <h1 className='bg-red-200'>Hello World</h1>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>AVIJIT KARMAKER</h1>
+        <h2>Full Stack Developer</h2>
+        <p>Crafting Digital Experiences, One Line of Code at a Time.</p>
+    </div>
+  )
+}
+
+export default Header
