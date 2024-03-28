2e15d7a9354a2801254dcdad2aeffc6bb59c09de
Avijit Karmaker <ajkacca@gmail.com>
2024-03-28 11:31:31 +0200

    update transitioning effect in navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
   return (
     <>
         <nav className='flex justify-center gap-3 items-center p-4 bg-gray-800 text-white my-4'>
-            <NavLink to='/'>About</NavLink>
-            <NavLink to='/projects'>Projects</NavLink>
-            <NavLink to='/experience'>Experience</NavLink>
+            <NavLink to='/' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-slate-200 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">About</NavLink>
+            <NavLink to='/projects' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-slate-200 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">Projects</NavLink>
+            <NavLink to='/experience' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-slate-200 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">Experience</NavLink>
         </nav>
     </>
   )
