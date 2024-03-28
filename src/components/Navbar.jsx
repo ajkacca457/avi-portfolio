9c3e137282508ec20bef164f9c57b6f7af27e0f2
Avijit Karmaker <ajkacca@gmail.com>
2024-03-28 12:45:00 +0200

    add link with company name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-        <nav className='flex justify-center gap-3 items-center p-4 bg-gray-800 text-white my-4'>
+        <nav className='flex justify-center gap-3 items-center p-4 bg-gray-800 text-slate-400 my-4'>
             <NavLink to='/' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-slate-200 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">About</NavLink>
             <NavLink to='/projects' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-slate-200 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">Projects</NavLink>
             <NavLink to='/experience' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-slate-200 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">Experience</NavLink>
