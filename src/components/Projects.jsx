e0b70fea6b3425473c2c5a4a04055706129e1dbe
Avijit Karmaker <ajkacca@gmail.com>
2024-03-30 23:15:16 +0200

    update project images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,26 +1,36 @@
 import React from 'react'
+import Netox from '../assets/images/netox.png'
+import Endomines from '../assets/images/endomines.png'
+import Efecte from '../assets/images/efecte.png'
+import IcareMap from '../assets/images/icare-map.png'
 
 const projectsData = [
     {
-        image:"https://fakeimg.pl/600x400",
-        title: 'Icare interactive map',
+        image:Netox,
+        title: 'netox.com',
         description: 'Icare interactive map',
         stack: ["Javascript", "React", "PHP", "Wordpress", "Tailwind", "Scss", "Bootstrap", "Html", "Css", "Node.js", "Typescript"]
     },
     {
-        image:"https://fakeimg.pl/600x400",
-        title: 'Icare manual downloader',
+        image:Efecte,
+        title: 'efecte.tenderoffer.fi',
         description: 'Description 2',
         stack: ["Javascript", "React", "PHP", "Wordpress", "Tailwind", "Scss", "Bootstrap", "Html", "Css", "Node.js", "Typescript"]
     },
     {
-        image:"https://fakeimg.pl/600x400",
-        title: 'Rightware.com',
+        image:Endomines,
+        title: 'endomines.com',
+        description: 'Description 2',
+        stack: ["Javascript", "React", "PHP", "Wordpress", "Tailwind", "Scss", "Bootstrap", "Html", "Css", "Node.js", "Typescript"]
+    },
+    {
+        image:IcareMap,
+        title: 'Icare interactive map',
         description: 'Description 3',
         stack: ["Javascript", "React", "PHP", "Wordpress", "Tailwind", "Scss", "Bootstrap", "Html", "Css", "Node.js", "Typescript"]
     },
     {
-        image:"https://fakeimg.pl/600x400",
+        image:"./assets/images/efecte0.png",
         title: 'Endomines.com',
         description: 'Description 4',
         stack: ["Javascript", "React", "PHP", "Wordpress", "Tailwind", "Scss", "Bootstrap", "Html", "Css", "Node.js", "Typescript"]
@@ -46,9 +56,9 @@ const Projects = () => {
             const { image, title, description, stack } = project;
 
             return (
-                <div key={index} className='my-4 grid grid-cols-4 gap-x-6 px-4 py-2 group hover:bg-slate-800 transition duration-250 ease-out hover:ease-in'>
+                <div key={index} className='my-4 grid grid-cols-4 gap-x-6 px-4 py-2 group hover:bg-slate-800 transition duration-250 ease-out hover:ease-in items-start'>
                     <div className='col-span-1'>
-                        <img src={image} alt={title} className='w-full h-full object-cover' />
+                        <img src={image} alt={title} className='w-full h-full object-contain' />
                     </div>
                     <div className='col-span-3'>
                         <p className='text-lg mb-2'>{title}</p>
