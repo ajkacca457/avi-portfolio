52f83a335bfd9f0e07e3f1b8785e7863ef3bddae
Avijit Karmaker <ajkacca@gmail.com>
2024-04-04 13:54:32 +0300

    update project link svg

--- src/components/HomeLayout.jsx
+++ src/components/HomeLayout.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
-
 const HomeLayout = () => {
+
+  
   return (
     <div className='max-w-content mx-auto relative'>
         <Header />
