c8b625193ce834b61f1a1e1817a14ab10394f6b7
Avijit Karmaker <ajkacca@gmail.com>
2024-04-04 11:44:12 +0300

    update fontsize with clamps

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ import Navbar from './Navbar'
 const Header = () => {
   return (
     <div className='text-center pt-8 pb-4 text-gray-300 sticky top-0 bg-slate-900'>
-        <h1 className='text-5xl'>AVIJIT KARMAKER</h1>
-        <h2 className='text-2xl'>Full Stack Developer</h2>
-        <p className='text-lg text-slate-400'>Crafting Digital Experiences, One Line of Code at a Time.</p>
+        <h1 className='text-heading'>AVIJIT KARMAKER</h1>
+        <h2 className='text-subheading'>Full Stack Developer</h2>
+        <p className='text-body-lg text-slate-400'>Crafting Digital Experiences, One Line of Code at a Time.</p>
         <Social />
         <Navbar />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,11 @@ export default {
       maxWidth: {
         'content': '1024px',
       },
+      fontSize: {
+        'heading': 'clamp(1.5rem, 0.5795rem + 2.7273vi, 2.625rem);',
+        'subheading':'clamp(1.125rem, 0.8182rem + 0.9091vi, 1.5rem);',
+        'body-lg':'clamp(0.875rem, 0.6705rem + 0.6061vi, 1.125rem);'
+      },
     },
   },
   plugins: [],
