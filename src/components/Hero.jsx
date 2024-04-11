f5ae87477722d7f8007d9453f29109a7f1976a98
Avijit Karmaker <ajkacca@gmail.com>
2024-04-11 21:06:36 +0300

    update bg and hero text tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <>
     <div className='text-center'>
         <h1 className='text-heading'>AVIJIT KARMAKER</h1>
-        <h2 className='text-subheading'>Full Stack Developer</h2>
+        <p className='text-subheading'>Full Stack Developer</p>
         <p className='text-body-lg text-slate-400'>Crafting Digital Experiences, One Line of Code at a Time.</p>
     </div>
     <Social/>
