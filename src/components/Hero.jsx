7085fb4bbc0c76a49d5fdbffa18fb124a1ec30ce
Avijit Karmaker <ajkacca@gmail.com>
2024-05-11 23:15:19 +0300

    add avatar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Social from './Social'
+import { AvatarMale } from './AvatarMale'
 
 const Hero = () => {
   return (
     <>
-      <div className='flex flex-col items-center h-screen'>
-        <h1 className='text-heading'>AVIJIT KARMAKER</h1>
-        <h2 className='text-subheading'>Full Stack Developer</h2>
-        <p className='text-body-lg text-slate-400'>Crafting Digital Experiences, One Line of Code at a Time.</p>
-        <Social/>
+      <div className='flex flex-col items-center h-full'>
+        <AvatarMale/>
       </div>
     </>
   )
