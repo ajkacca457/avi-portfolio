5e2c0bd5d7a37b6027e57d66f01ef85f3dead341
Avijit Karmaker <ajkacca@gmail.com>
2024-05-13 18:14:30 +0300

    remove unused component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Social from './Social'
 import { AvatarMale } from './AvatarMale'
+
 
 const Hero = () => {
   return (
     <>
       <div className='flex flex-col items-center h-full'>
-        <AvatarMale/>
+          <AvatarMale/>
       </div>
     </>
   )
