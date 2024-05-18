82669d5903f27db70c896a58ddc3255dc96f3491
Avijit Karmaker <ajkacca@gmail.com>
2024-05-18 20:33:41 +0300

    add asset and heading

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,10 +4,12 @@ import Hero from "../components/Hero";
 
 const About = () => {
   return (
+    <>
+    <h1>Hello i am Avi!</h1>
     <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }} style={{ height: '100vh' }}>
-      <color attach="background" args={["#ececec"]} />
       <Hero />
     </Canvas>
+    </>
   )
 }
 
