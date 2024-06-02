67fec3d85aeb86937f35ff0f9cad5b12d6218443
Avijit Karmaker <ajkacca@gmail.com>
2024-06-03 01:48:43 +0300

    add social to hero

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,15 @@
 import { Canvas } from "@react-three/fiber";
 import Hero from "../components/Hero";
+import Social from "../components/Social";
 
 
 const About = () => {
   return (
     <div className="grid grid-cols-2">
-    <div className="col-span-1 flex flex-col justify-center">
+    <div className="col-span-1 flex flex-col justify-around">
       <h1 className="text-4xl my-4">Hello, I am Avi!</h1>
       <p className="text-justify text-lg">My name is Avi and I am a full stack developer. I have experience in React, Node.js, Express, and MongoDB. I am currently learning how to use Three.js and React-Three-Fiber. I am excited to learn more about 3D modeling and animation. I am also interested in learning more about machine learning and artificial intelligence. I am looking forward to working on projects that involve these technologies.</p>
+      <Social />
     </div>
     <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }} style={{ height: '100vh' }} className="col-span-1">
       <Hero />
