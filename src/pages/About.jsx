dad29ceec9cca7ed829996fd20246360c7041ab3
Avijit Karmaker <ajkacca@gmail.com>
2024-07-21 12:53:46 +0300

    change fonts

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,9 +5,9 @@ import Social from "../components/Social";
 
 const About = () => {
   return (
-    <div className="grid grid-cols-2">
-    <div className="col-span-1 flex flex-col justify-around">
-      <h1 className="text-4xl my-4">Hello, I am Avi!</h1>
+    <div className="grid grid-cols-2 place-items-center">
+    <div className="col-span-1 flex flex-col">
+      <h1 className="text-4xl">Hello, I am Avi!</h1>
       <p className="text-justify text-lg">My name is Avi and I am a full stack developer. I have experience in React, Node.js, Express, and MongoDB. I am currently learning how to use Three.js and React-Three-Fiber. I am excited to learn more about 3D modeling and animation. I am also interested in learning more about machine learning and artificial intelligence. I am looking forward to working on projects that involve these technologies.</p>
       <Social />
     </div>
