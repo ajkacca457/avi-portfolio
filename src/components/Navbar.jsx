a48e3114c10f8e764598a7a30d1d66d48184e655
Avijit Karmaker <ajkacca@gmail.com>
2024-07-22 08:28:41 +0300

    update social icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-        <nav className='flex gap-3 items-center p-4 text-white my-4 rounded'>
-            <NavLink to='/' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-slate-200 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">About</NavLink>
+        <nav className='flex gap-3 items-center p-4 text-white text-lg my-4 rounded'>
+            <NavLink to='/' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-slate-200 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">Home</NavLink>
             <NavLink to='/projects' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-slate-200 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">Projects</NavLink>
             <NavLink to='/experience' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-slate-200 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">Experience</NavLink>
         </nav>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,8 +7,9 @@ const Landing = () => {
   return (
     <div className="grid grid-cols-2 place-items-center">
     <div className="col-span-1 flex flex-col">
-      <h1 className="text-5xl my-4">Hello, I am Avi!</h1>
-      <p className="text-justify text-2xl">My name is Avi and I am a full stack developer. I have experience in React, Node.js, Express, and MongoDB. I am currently learning how to use Three.js and React-Three-Fiber. I am excited to learn more about 3D modeling and animation. I am also interested in learning more about machine learning and artificial intelligence. I am looking forward to working on projects that involve these technologies.</p>
+      <p className="text-lg">Fullstack Developer</p>
+      <h1 className="text-7xl mt-4">Hello,<span className="text-purple-500">I am Avi!</span></h1>
+      <p className="text-justify text-xl my-8">I build dynamic, responsive web apps using Wordpress,PHP, MongoDB, Express.js, React.js, and Node.js. Check out my projects to see my work in action.</p>
       <Social />
     </div>
     <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }} style={{ height: '75vh' }} className="col-span-1">
@@ -18,4 +19,4 @@ const Landing = () => {
   )
 }
 
-export default Landing
+export default Landing;
