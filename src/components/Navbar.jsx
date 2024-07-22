798c1db992a5f2c009264f11fc5a6a572908eaa5
Avijit Karmaker <ajkacca@gmail.com>
2024-07-22 09:07:48 +0300

    update navlink hover state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,56 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-        <nav className='flex gap-x-10 items-center p-4 text-white text-lg my-4 rounded'>
-            <NavLink to='/' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] transition duration-300 hover:text-purple-600 after:bg-purple-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">Home</NavLink>
-            <NavLink to='/work' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] transition duration-300 hover:text-purple-600 after:bg-purple-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">Work</NavLink>
-            <NavLink to='/resume' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] transition duration-300 hover:text-purple-600 after:bg-purple-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">Resume</NavLink>
-            <NavLink to='/contact' className="relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] transition duration-300 hover:text-purple-600 after:bg-purple-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">Contact</NavLink>
-        </nav>
-    </>
-  )
-}
+      <nav className="flex gap-x-10 items-center p-4 text-white text-lg my-4 rounded">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] transition duration-300 ${
+              isActive ? "text-purple-600 after:scale-x-100" : "hover:text-purple-600"
+            } after:bg-purple-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center`
+          }
+        >
+          Home
+        </NavLink>
 
-export default Navbar
+        <NavLink
+          to="/work"
+          className={({ isActive }) =>
+            `relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] transition duration-300 ${
+              isActive ? "text-purple-600 after:scale-x-100" : "hover:text-purple-600"
+            } after:bg-purple-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center`
+          }
+        >
+          Work
+        </NavLink>
+
+        <NavLink
+          to="/resume"
+          className={({ isActive }) =>
+            `relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] transition duration-300 ${
+              isActive ? "text-purple-600 after:scale-x-100" : "hover:text-purple-600"
+            } after:bg-purple-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center`
+          }
+        >
+          Resume
+        </NavLink>
+
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            `relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] transition duration-300 ${
+              isActive ? "text-purple-600 after:scale-x-100" : "hover:text-purple-600"
+            } after:bg-purple-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center`
+          }
+        >
+          Contact
+        </NavLink>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
