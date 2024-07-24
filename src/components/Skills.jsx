648d610eb412c2fff530669d7854e04481808318
Avijit Karmaker <ajkacca@gmail.com>
2024-07-25 00:32:01 +0300

    update skills card

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 import { AiOutlineHtml5 } from "react-icons/ai";
 import { TiCss3 } from "react-icons/ti";
 import { SiJavascript } from "react-icons/si";
+import { BsFiletypeScss } from "react-icons/bs";
+import { SiTailwindcss } from "react-icons/si";
+import { FaBootstrap } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { FaNode } from "react-icons/fa6";
+import { FaVuejs } from "react-icons/fa6";
+import { SiPhp } from "react-icons/si";
+import { SiRubyonrails } from "react-icons/si";
+import { FaWordpress } from "react-icons/fa";
+
+
 
 
 
@@ -35,18 +46,58 @@ const Skills = () => {
                 <SiJavascript className='text-purple-500 text-5xl'/>
             </span>
         </div>
-        <div className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 px-3 py-2 transition-all" href="#">
+        <div className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-purple-500 px-3 py-2 transition-all" href="#">
             <span className="my-6 grid h-24 w-24 place-items-center">
+                <BsFiletypeScss className='text-purple-500 text-5xl' />
             </span>
         </div>
-        <div className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 px-3 py-2 transition-all" href="#">
+        <div className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-purple-500 px-3 py-2 transition-all" href="#">
             <span className="my-6 grid h-24 w-24 place-items-center">
+                <SiTailwindcss  className='text-purple-500 text-5xl' />
             </span>
         </div>
-        <div className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 px-3 py-2 transition-all" href="#">
+        <div className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-purple-500 px-3 py-2 transition-all" href="#">
             <span className="my-6 grid h-24 w-24 place-items-center">
+                <FaBootstrap className='text-purple-500 text-5xl' />
             </span>
         </div>
+
+        <div className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-purple-500 px-3 py-2 transition-all" href="#">
+            <span className="my-6 grid h-24 w-24 place-items-center">
+                <FaReact className='text-purple-500 text-5xl' />
+            </span>
+        </div>
+
+        <div className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-purple-500 px-3 py-2 transition-all" href="#">
+            <span className="my-6 grid h-24 w-24 place-items-center">
+                <FaVuejs className='text-purple-500 text-5xl' />
+            </span>
+        </div>
+
+        <div className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-purple-500 px-3 py-2 transition-all" href="#">
+            <span className="my-6 grid h-24 w-24 place-items-center">
+                <FaNode className='text-purple-500 text-6xl' />
+            </span>
+        </div>
+
+        <div className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-purple-500 px-3 py-2 transition-all" href="#">
+            <span className="my-6 grid h-24 w-24 place-items-center">
+                <SiPhp className='text-purple-500 text-6xl' />
+            </span>
+        </div>
+
+        <div className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-purple-500 px-3 py-2 transition-all" href="#">
+            <span className="my-6 grid h-24 w-24 place-items-center">
+                <SiRubyonrails className='text-purple-500 text-6xl' />
+            </span>
+        </div>
+
+        <div className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-purple-500 px-3 py-2 transition-all" href="#">
+            <span className="my-6 grid h-24 w-24 place-items-center">
+                <FaWordpress className='text-purple-500 text-6xl' />
+            </span>
+        </div>
+
         </div>
     </div>
 </div>
