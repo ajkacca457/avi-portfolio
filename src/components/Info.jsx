3010e9e6503ca2cc16b1468ebcf79a612dd36b96
Avijit Karmaker <ajkacca@gmail.com>
2024-07-24 10:46:26 +0300

    update info style

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -10,25 +10,25 @@ const Info = () => {
   return (
     <div className='w-full p-8'>
         <div className='flex my-8 w-3/4 mx-auto'>
-            <FaPhone className="text-6xl text-purple-600 p-4 bg-slate-700 mr-6"/>
-            <div>
-                <p>Phone</p>
+            <FaPhone className="text-6xl text-purple-500 p-4 bg-slate-700 mr-6"/>
+            <div className='flex flex-col justify-between'>
+                <p className='text-s text-purple-400'>Phone</p>
                 <p className="text-xl">+1 234 567 890</p>
             </div>
         </div>
 
         <div className='flex my-8 w-3/4 mx-auto'>
-            <SiMinutemailer className="text-6xl text-purple-600 p-4 bg-slate-700 mr-6"/>
-            <div>
-                <p>Email</p>
+            <SiMinutemailer className="text-6xl text-purple-500 p-4 bg-slate-700 mr-6"/>
+            <div className='flex flex-col justify-between'>
+                <p className='text-s text-purple-400'>Phone</p>
                 <p className="text-xl">+1 234 567 890</p>
             </div>
         </div>
 
         <div className='flex my-8 w-3/4 mx-auto'>
-            <FaLocationDot className="text-6xl text-purple-600 p-4 bg-slate-700 mr-6"/>
-            <div>
-                <p>Location</p>
+            <FaLocationDot className="text-6xl text-purple-500 p-4 bg-slate-700 mr-6"/>
+            <div className='flex flex-col justify-between'>
+                <p className='text-s text-purple-400'>Phone</p>
                 <p className="text-xl">+1 234 567 890</p>
             </div>
         </div>
