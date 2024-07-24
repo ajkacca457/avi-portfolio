1019653a7592fe841e74b9ce7b942603a7addd5a
Avijit Karmaker <ajkacca@gmail.com>
2024-07-24 10:49:03 +0300

    update contact form style

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -13,23 +13,23 @@ const Info = () => {
             <FaPhone className="text-6xl text-purple-500 p-4 bg-slate-700 mr-6"/>
             <div className='flex flex-col justify-between'>
                 <p className='text-s text-purple-400'>Phone</p>
-                <p className="text-xl">+1 234 567 890</p>
+                <p className="text-xl">(+358) 465 201 355</p>
             </div>
         </div>
 
         <div className='flex my-8 w-3/4 mx-auto'>
             <SiMinutemailer className="text-6xl text-purple-500 p-4 bg-slate-700 mr-6"/>
             <div className='flex flex-col justify-between'>
-                <p className='text-s text-purple-400'>Phone</p>
-                <p className="text-xl">+1 234 567 890</p>
+                <p className='text-s text-purple-400'>Email</p>
+                <p className="text-xl">ajkacca@gmail.com</p>
             </div>
         </div>
 
         <div className='flex my-8 w-3/4 mx-auto'>
             <FaLocationDot className="text-6xl text-purple-500 p-4 bg-slate-700 mr-6"/>
             <div className='flex flex-col justify-between'>
-                <p className='text-s text-purple-400'>Phone</p>
-                <p className="text-xl">+1 234 567 890</p>
+                <p className='text-s text-purple-400'>Address</p>
+                <p className="text-xl">Kivistö, Vantaa, Finland, 01700 </p>
             </div>
         </div>
 
