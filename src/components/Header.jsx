eae6e07ee9e1a99b4b1db5d3a8ca712baac722ab
Avijit Karmaker <ajkacca@gmail.com>
2024-07-26 11:59:01 +0300

    update skills section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
-    <div className='fixed w-full z-50 left-0 box-border'>
+    <div className='fixed w-full z-50 left-0 box-border bg-gradient-to-l from-slate-800 via-slate-950 to-slate-900'>
       <div className='flex justify-between items-center max-w-content-large mx-auto'>
         <h1 className='hidden m768:block mx-4 text-logo-heading'>Avi<span className='text-purple-600'>.</span></h1>
         <Navbar />
--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -34,6 +34,7 @@ const MobileNav = () => {
                 : "hover:text-purple-500"
             } after:bg-purple-500 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center`
           }
+          onClick={() => setIsOpen(false)}
         >
           Home
         </NavLink>
@@ -47,6 +48,7 @@ const MobileNav = () => {
                 : "hover:text-purple-500"
             } after:bg-purple-500 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center`
           }
+          onClick={() => setIsOpen(false)}
         >
           Work
         </NavLink>
@@ -60,6 +62,7 @@ const MobileNav = () => {
                 : "hover:text-purple-500"
             } after:bg-purple-500 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center`
           }
+          onClick={() => setIsOpen(false)}
         >
           Resume
         </NavLink>
@@ -73,6 +76,7 @@ const MobileNav = () => {
                 : "hover:text-purple-500"
             } after:bg-purple-500 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center`
           }
+          onClick={() => setIsOpen(false)}
         >
           Contact
         </NavLink>
