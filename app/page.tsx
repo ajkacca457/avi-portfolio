4a887156100a32c5c929df82b4bb00225f752a63
Avijit Karmaker <ajkacca@gmail.com>
2024-08-05 09:04:58 +0300

    add bentogrid

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Hero from "./components/Hero";
+import { BentoGridThirdDemo } from "./components/Grid";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden">
+    <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden bg-black">
       <Hero />
+      <BentoGridThirdDemo />
     </main>
   );
 }
