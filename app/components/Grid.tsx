7623d4d21c6e7dddd814d0d70b7d2f3a837ebc46
Avijit Karmaker <ajkacca@gmail.com>
2024-08-08 00:07:22 +0300

    update services

--- app/components/Grid.tsx
+++ app/components/Grid.tsx
@@ -123,18 +123,18 @@ return (
 const SkeletonFour = () => {
   const first = {
     initial: {
-      x: 20,
-      rotate: -5,
+      x: 10,
+      rotate:3,
     },
     hover: {
-      x: 0,
+      x: 10,
       rotate: 0,
     },
   };
   const second = {
     initial: {
-      x: -20,
-      rotate: 5,
+      x: -10,
+      rotate:3,
     },
     hover: {
       x: 0,
@@ -146,28 +146,22 @@ const SkeletonFour = () => {
       initial="initial"
       animate="animate"
       whileHover="hover"
-      className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-row space-x-2"
+      className=" w-full h-full mt-4 min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-row space-x-2"
     >
       <motion.div
         variants={first}
-        className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
+        className="w-full rounded-2xl text-white bg-purple-400/70 p-4 mb-2 mt-2 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
       >
-        <p className="border border-red-500 bg-red-100 dark:bg-red-900/20 text-red-600 text-xs rounded-full px-2 py-0.5 mt-4">
-          Web Design
-        </p>
+       Web Design
       </motion.div>
-      <motion.div className="h-full relative z-20 w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center">
-        <p className="border border-green-500 text-center bg-green-100 dark:bg-green-900/20 text-green-600 text-xs rounded-full px-2 py-0.5 mt-4">
-          Web Development
-        </p>
+      <motion.div className="w-full relative text-white z-20 mb-2 rounded-2xl bg-purple-400/70 p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center">
+        Web Development
       </motion.div>
       <motion.div
         variants={second}
-        className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
+        className="w-full rounded-2xl text-white bg-purple-400/70 p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
       >
-        <p className="border border-orange-500 text-center bg-orange-100 dark:bg-orange-900/20 text-orange-600 text-xs rounded-full px-2 py-0.5 mt-4">
-          Mobile Development
-        </p>
+        Application Development
       </motion.div>
     </motion.div>
   );
