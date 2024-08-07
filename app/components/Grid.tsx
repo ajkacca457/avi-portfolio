aa4bd8c8076616e35b5de1663012b9190d2d88df
Avijit Karmaker <ajkacca@gmail.com>
2024-08-07 15:51:26 +0300

    remove moto from about me

--- app/components/Grid.tsx
+++ app/components/Grid.tsx
@@ -70,7 +70,6 @@ const SkeletonTwo = () => {
       <div className="h-full w-full absolute top-0 left-0 bg-black/60 z-10"></div>
 
       <motion.div className="h-full w-full rounded-lg p-4 flex flex-col justify-center gap-y-4 z-20">
-        <motion.h1 variants={{ hover: { color: "#8B5CF6" } }} className="text-white font-bold text-2xl text-center">Determination wins battles. Perseverance wins wars.</motion.h1>
         <p className="text-white text-justify text-sm">Hi, I’m Avi. I enjoy learning new technologies, building projects, and studying Finnish. With four years as a Programme/Project Manager in NGOs and the corporate sector, I've worked on community development, HR, and IT projects like e-learning. After self-learning programming, I completed an intensive bootcamp at Microverse. Now, as a Full Stack Developer, I’m eager to work with innovative teams to create impactful projects.</p>
       </motion.div>
     </motion.div>
