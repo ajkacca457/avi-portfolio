a5d95aa96e759ef178fb15bd55ac836293a14449
Avijit Karmaker <ajkacca@gmail.com>
2024-08-11 20:34:38 +0300

    update project

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -13,13 +13,13 @@ const Projects = () => {
             title: 'netox.com',
             url: 'https://netox.com',
             description: 'The website is constructed using custom Gutenberg React blocks, with my primary responsibility being the creation of these custom blocks designed to perform various interactive activities. Throughout this project, our team collectively devised unique solutions tailored to Gutenberg blocks. We successfully implemented several npm packages with custom Gutenberg React blocks and brainstormed innovative ideas to optimize their efficiency. Notably, we effectively utilized GSAP with custom blocks to introduce animation and interactivity. Our collaborative efforts resulted in a diverse array of custom blocks, enhancing user engagement and contributing to the overall success of the site.',
-            stack: ["Javascript", "React", "PHP", "Wordpress", "Scss", "Gutenberg-blocks"]
+            stack: ["Javascript", "React", "PHP", "Wordpress", "Scss", "Gutenberg Blocks"]
         },
         {
             title: 'Icare interactive map',
             url: 'https://patients.icare-world.com/find-a-provider',
             description: 'Description 3',
-            stack: ["Javascript", "Google-Maps-API", "PHP", "Wordpress", "Scss","Typescript"]
+            stack: ["Javascript", "PHP", "Wordpress", "Scss","Typescript"]
         },
         {
             title: 'rightware.com',
@@ -40,14 +40,14 @@ const Projects = () => {
                         <CardBody className="relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]  border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                             <CardItem
                                 translateZ="50"
-                                className="text-xl font-bold text-blue-500"
+                                className="text-xl font-bold text-purple-300"
                             >
                                 {project.title}
                             </CardItem>
                             <CardItem
                                 as="p"
                                 translateZ="60"
-                                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                                className="text-white"
                             >
                                 Hover over this card to unleash the power of CSS perspective
                             </CardItem>
@@ -65,13 +65,9 @@ const Projects = () => {
                                     as={Link}
                                     href="https://twitter.com/mannupaaji"
                                     target="__blank"
-                                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white block"
+                                    className="px-4 py-2 rounded-xl text-xs font-normal mt-4 block text-purple-300 mb-4"
                                 >
-                                    {project.stack.map((stack, index) => {
-                                        return (
-                                            <span key={index} className="mr-2">{stack}</span>
-                                        )
-                                    })} 
+                                    {project.stack.join(", ")}
                                 </CardItem>
                                 <CardItem
                                     translateZ={20}
