c201519d23641b6b82c13e5596b52f9db3deefe9
Avijit Karmaker <ajkacca@gmail.com>
2024-08-17 11:23:02 +0300

    update all projects margin

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,7 @@ import AllProjects from '../components/AllProjects'
 const ProjectPage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden bg-black text-white">
-      <h1 className='text-white text-2xl md:text-3xl lg:text-5xl leading-snug tracking-wide px-2 text-center font-bold'>My <span className='text-purple-300'>Projects</span></h1>
+      <h1 className='text-white text-2xl md:text-3xl lg:text-5xl leading-snug tracking-wide px-2 text-center font-bold mt-[10vh]'>All <span className='text-purple-300'>Projects</span></h1>
       <AllProjects />
     </main>
   )
