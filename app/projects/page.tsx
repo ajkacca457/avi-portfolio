c41a394b220a23ac86cce35ca9ee575ade0c0766
Avijit Karmaker <ajkacca@gmail.com>
2024-08-17 11:56:41 +0300

    update home button for projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import React from 'react'
 import AllProjects from '../components/AllProjects'
+import Link from 'next/link'
 
 const ProjectPage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden bg-black text-white">
-      <h1 className='text-white text-2xl md:text-3xl lg:text-5xl leading-snug tracking-wide px-2 text-center font-bold mt-[10vh]'>All <span className='text-purple-300'>Projects</span></h1>
+      <Link href='/' className='fixed left-10 top-[20px] z-50'>Back to home</Link>
+      <h1 className='text-white text-2xl md:text-3xl lg:text-5xl leading-snug tracking-wide px-2 text-center font-bold mt-[5vh]'>All <span className='text-purple-300'>Projects</span></h1>
       <AllProjects />
     </main>
   )
