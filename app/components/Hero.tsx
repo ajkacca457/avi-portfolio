65e1284ff9d8ecca52625edb9edaed07b6347448
Avijit Karmaker <ajkacca@gmail.com>
2024-08-18 17:55:55 +0300

    reduce animation

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from 'react'
-import { SparklesCore } from './ui/sparkles'
 import { TextGenerateEffect } from './ui/text-generate-effect'
 import { Spotlight } from './ui/Spotlight';
 import Link from 'next/link';
@@ -23,16 +22,7 @@ const Hero = () => {
                 <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
                 <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
                 <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
-
-                {/* Core component */}
-                <SparklesCore
-                    background="transparent"
-                    minSize={0.2}
-                    maxSize={1}
-                    particleDensity={100}
-                    className="w-full h-full"
-                    particleColor="#0066b2"
-                />
+                
                 {/* Radial Gradient to prevent sharp edges */}
                 <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
             </div>
@@ -43,7 +33,7 @@ const Hero = () => {
             <p className='text-white mt-10 text-xl'>Hi! I am a Full stack developer from Finland.</p>
 
             <Link href={`https://docs.google.com/document/d/1PelJIXVSGFz_4oKLre-k8oOjpLNcnID6zxSuQ6ezOZU/edit?usp=sharing`} target='_blank' className="relative inline-flex h-12 mt-10 overflow-hidden rounded-sm p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                <span className="absolute inset-[-1000%] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-sm bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
                     Download CV
                 </span>
@@ -56,15 +46,6 @@ const Hero = () => {
                 <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
                 <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
 
-                {/* Core component */}
-                <SparklesCore
-                    background="transparent"
-                    minSize={0.2}
-                    maxSize={1}
-                    particleDensity={100}
-                    className="w-full h-full"
-                    particleColor="#0066b2"
-                />
                 {/* Radial Gradient to prevent sharp edges */}
                 <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
             </div>
