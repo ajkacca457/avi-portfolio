7e0452dfc7b2a467812193278f00636d5542a6cb
Avijit Karmaker <ajkacca@gmail.com>
2024-08-19 12:47:24 +0300

    update input name properties

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -14,7 +14,7 @@ const Form = () => {
                     Full Name
                 </label>
                 <input
-                    name="Name"
+                    name="name"
                     type="text"
                     placeholder="First and Last Name"
                     className="border border-gray-400 py-2 px-3 w-full rounded-md focus:outline-none focus:border-purple-600 text-sm md:text-base"
@@ -25,7 +25,7 @@ const Form = () => {
                     Email
                 </label>
                 <input
-                    name="Email"
+                    name="email"
                     type="email"
                     placeholder="Your email"
                     className="border border-gray-400 py-2 px-3 w-full rounded-md focus:outline-none focus:border-purple-600 text-sm md:text-base"
@@ -36,7 +36,7 @@ const Form = () => {
                     Message
                 </label>
                 <textarea
-                    name="Message"
+                    name="message"
                     rows={4}
                     placeholder="Enter your message here"
                     className="border border-gray-400 py-2 px-3 w-full rounded-md focus:outline-none focus:border-purple-600 text-sm md:text-base"
