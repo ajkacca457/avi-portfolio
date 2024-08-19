9614f720024bcdee6d37edceabbea38e36a4062f
Avijit Karmaker <ajkacca@gmail.com>
2024-08-19 12:35:06 +0300

    add copyright

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+
+        <footer className="text-purple-200 text-center p-4 w-full text-sm">
+            <p>© 2023 Avijit Karmaker</p>
+        </footer>
+
+    )
+}
+
+export default Footer
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden bg-black">
@@ -14,6 +15,7 @@ export default function Home() {
       <Experience />
       <Projects />
       <Contact />
+      <Footer />
     </main>
   );
 }
