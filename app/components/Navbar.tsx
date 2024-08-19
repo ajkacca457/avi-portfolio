53cf6f9e28ca870961c3beb7ad5a8f2fcf58ba2e
Avijit Karmaker <ajkacca@gmail.com>
2024-08-19 10:52:27 +0300

    update services section with video opacity

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
   return (
     <div className='w-full fixed top-10 z-50'>
-        <div className="flex justify-center items-center py-2 px-6 gap-x-6 bg-gradient-to-b from-blue-900 via-slate-900 to-stone-800 text-white w-fit mx-auto rounded-3xl border-[1px] border-blue-400 text-xl">
+        <div className="flex justify-center items-center py-2 px-6 gap-x-6 bg-gradient-to-b from-slate-800 via-slate-950 to-slate-800 text-white w-fit mx-auto rounded-3xl border-[1px] border-blue-300 text-xl">
             <button onClick={()=> handleScroll("hero")}><TiHome className='block md:hidden'/> <span className='hidden md:block text-sm'>Home</span></button>
             <button onClick={()=>handleScroll("bento-grid-scroll")}><BsInfoCircleFill className='block md:hidden'/> <span className='hidden md:block text-sm'>About Me</span></button>
             <button onClick={()=>handleScroll("experience")}><MdWorkHistory  className='block md:hidden'/> <span className='hidden md:block text-sm'>Experience</span></button>
